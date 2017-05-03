7a6e53f4c98bb6173872a7b6a6cdca26b0ec5490
VatesaniS <vatesanis@gmail.com>
2017-05-03 10:02:07 +0200

    add Controllers

---
+++ app/src/main/java/fr/eni/eniprojectandroid/controllers/VehiculeActivity.java
@@ -0,0 +1,15 @@
+package fr.eni.eniprojectandroid.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.eni.eniprojectandroid.R;
+
+public class VehiculeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicule);
+    }
+}
